2487e2e524d32ef7395e2f27d5afa0336d359db9
kongooo <1184701824@qq.com>
2017-07-22 22:13:54 +0800

    again

---
+++ 吃豆人/Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacmanMove : MonoBehaviour {
+
+	public float speed = 0.4f;
+
+	Vector2 dest = Vector2.zero;
+
+
+	void Start () {
+	
+		dest = transform.position;
+
+	}
+
+	void FixedUpdate () {
+
+
+		//通过Rigidbody2D，实现Pacman的移动
+		Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+
+		GetComponent<Rigidbody2D>().MovePosition(p);
+
+		//收集输入信息，通过Valid方法，计算移动距离
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+
+			dest=Valid (Vector2.up,dest);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+
+			dest=Valid (Vector2.right,dest);
+		}
+			
+
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+
+			dest=Valid (-Vector2.up,dest);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+
+			dest=Valid (-Vector2.right,dest);
+		}
+
+
+		//判断执行动画
+		Vector2 dir = dest - (Vector2)transform.position;
+
+		GetComponent<Animator> ().SetFloat ("DirX",dir.x);
+
+		GetComponent<Animator> ().SetFloat ("DirY",dir.y);
+			
+	}
+		
+
+	//该方法用来判断角色是否可以在该方向上移动，同时计算移动距离
+	Vector2 Valid(Vector2 dir,Vector2 dest)
+	{
+
+		//通过射线检测判断是否可以通过，移动时可能会有一定误差，所以我用Vector2.Scale(dir,new Vector2(0.1f,0.1f))进行了一定误差处理）
+
+		Vector2 pos =transform.position;
+
+		RaycastHit2D hit = Physics2D.Linecast (pos+dir+Vector2.Scale(dir,new Vector2(0.1f,0.1f)),pos);
+
+
+		//通过相应判断进行移动计算
+		if(hit.collider==GetComponent<Collider2D>())
+		{
+
+			if((Vector2)transform.position == dest)
+			{
+				
+				dest = (Vector2)transform.position + dir;
+
+			}
+
+		}
+
+		return dest;
+	}
+		
+}
---
+++ 吃豆人/Assets/grid.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid : MonoBehaviour {
+
+    public Transform StartTransform;
+    public Transform EndTransform;
+
+    public float NodeRadius=0.5f;        //节点半径
+
+    public LayerMask WallLayer;      //过滤墙体所在的层
+
+    public class NodeItem              //节点类 储存每一个节点的信息
+    {
+        public bool isWall;
+        public int x, y;          //节点坐标
+        public Vector3 pos;       //位置
+        public int gCost;         //起点花费
+        public int hCost;         //终点花费
+        public int fCost          //总花费
+        {
+            get
+            {
+                return gCost + fCost;
+            }
+        }        
+        public NodeItem parent;    //父节点（中心节点）
+        public NodeItem(bool  Iswall,int X,int Y,Vector3 Pos)    //初始化
+        {
+            this.x = X;
+            this.y = Y;
+            this.isWall = Iswall;
+            this.pos = Pos;
+        }
+    }
+
+    private NodeItem[,] Grid;    //二维数组
+
+    private int w, h;            
+
+    private GameObject Wall;
+    private GameObject Path;
+
+    private List<GameObject> PathObject = new List<GameObject>();
+
+	void Awake ()
+    {
+        w = Mathf.RoundToInt(transform.localScale.x * 28);   
+        h = Mathf.RoundToInt(transform.localScale.y * 31);   
+        Grid = new NodeItem[w, h];                           //创建对应的节点二维数组
+
+         Wall = new GameObject("WallRange");
+         Path = new GameObject("PathRange");
+
+        for(int x=2;x<w;x++)
+            for(int y = 2; y < h; y++)
+            {
+                Vector3 pos = new Vector3(x, y, 0);
+                bool isWall = Physics.CheckSphere(pos, NodeRadius,WallLayer);
+                Grid[x, y] = new NodeItem(isWall, x, y, pos);      //构建节点
+                if (isWall)
+                {
+                   //画出不可行走区域？
+                }
+            }
+	}
+    //根据坐标位置获得对应的节点
+    public NodeItem GetGrid(Vector3 position)
+    {
+        //取得坐标的近似整数作为节点的序列
+        int x = Mathf.RoundToInt(position.x);
+        int y = Mathf.RoundToInt(position.y);
+        //限定x y范围
+        x = Mathf.Clamp(x, 2, w - 1);
+        y = Mathf.Clamp(y, 2, h - 1);
+        return Grid[x, y];
+    }
+	
+	
+	void Update () {
+		
+	}
+}
