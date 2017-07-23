ad7df11d3f93715359ab94254aa0e80aec800b60
kongooo <1184701824@qq.com>
2017-07-23 19:23:04 +0800

    保存

---
+++ 吃豆人/Assets/LoadPath.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPath : MonoBehaviour {
+
+    private grid Grid;
+	
+	void Start () {
+        Grid = GetComponent<grid>();
+	}
+	
+	
+	void Update () {
+        changepath(Grid.StartTransform.position, Grid.EndTransform.position);
+	}
+
+    void changepath(Vector3 s,Vector3 e)
+    {
+        grid.NodeItem StartNode = Grid.GetGrid(s);     //将开始和结束的坐标转换成节点坐标
+        grid.NodeItem EndNode = Grid.GetGrid(e);
+
+        //定义开启列表和关闭列表
+        //开启列表：存储可以成为下一个父节点的节点
+        //关闭列表：存储成为过父节点的节点
+        List<grid.NodeItem> OpenList = new List<grid.NodeItem>();
+        List<grid.NodeItem> CloseList = new List<grid.NodeItem>();
+
+        OpenList.Add(StartNode);
+
+        while (OpenList.Count > 0)
+        {
+            //将开启列表的第一个节点记录下来
+            grid.NodeItem ParentNode = OpenList[0];
+
+            //遍历开启列表寻找parent
+            for(int i = 0; i < OpenList.Count; i++)
+            {
+                //若开启列表中的节点的总花费更小或不变并且到终点的花费更小
+                if (OpenList[i].fCost <= ParentNode.fCost && OpenList[i].gCost < ParentNode.gCost)
+                {
+                    ParentNode = OpenList[i];
+                }
+            }
+
+            //将找到的父节点从开启列表中删除，加入关闭列表
+            OpenList.Remove(ParentNode);
+            CloseList.Add(ParentNode);
+
+            //若父节点为终点节点则停止更新路程花费并且生成路径
+            if (ParentNode == EndNode)
+            {
+                //生成路径
+                ShowPath(StartNode, EndNode);
+                return;
+            }
+
+            List<grid.NodeItem> nodeneighborhood = Grid.GetNearNode(ParentNode);
+            //更新花费
+            for(int i = 0; i < nodeneighborhood.Count; i++)
+            {
+                //跳过在墙中的节点和已经关闭的节点
+                if (nodeneighborhood[i].isWall || CloseList.Contains(ParentNode))
+                {
+                    continue;
+                }
+                //得到更新后的花费
+                int newcost = ParentNode.gCost + updatecost(nodeneighborhood[i], ParentNode);
+                //若更新后花费更少或者该节点原来不在开启节点中则更新
+                if (newcost < nodeneighborhood[i].gCost || !OpenList.Contains(nodeneighborhood[i]))
+                {
+                    //更新到起点花费
+                    nodeneighborhood[i].gCost = newcost;
+                    //更新到终点花费
+                    nodeneighborhood[i].hCost = updatecost(nodeneighborhood[i], EndNode);
+                    //更新父节点
+                    nodeneighborhood[i].parent = ParentNode;
+                    //若该节点不在开启节点中就把它放进去
+                    if (!OpenList.Contains(nodeneighborhood[i]))
+                    {
+                        OpenList.Add(nodeneighborhood[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    //生成路径为从结束节点处开始依次寻找其父节点直到开始节点处停止
+    void ShowPath(grid.NodeItem startnode,grid.NodeItem endnode)
+    {
+        //定义存储路径的列表
+        List<grid.NodeItem> path = new List<grid.NodeItem>();
+        //当结束节点不为空时
+        if (endnode != null)
+        {
+            //存储父节点的变量
+            grid.NodeItem temp = endnode;
+            //当没有达到开始节点时
+            while (temp != startnode)
+            {
+                //向存储路径的列表中添加父节点
+                path.Add(temp);
+                //更新父节点
+                temp = temp.parent;
+            }
+            //反转路径（之前是一条从结束节点到开始节点的路）
+            path.Reverse();
+        }
+        //更新路径
+        Grid.upadatePath(path);
+    }
+
+    int updatecost(grid.NodeItem s,grid.NodeItem e)
+    {
+        int x = Mathf.Abs(s.x - e.x);
+        int y = Mathf.Abs(s.y - e.y);
+        if (x > y)
+        {
+            return y * 10 + (x - y) * 14;
+        }
+        return x * 10 + (y - x) * 14;
+    }
+}
--- 吃豆人/Assets/grid.cs
+++ 吃豆人/Assets/grid.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class grid : MonoBehaviour {
 
+    public GameObject node;       //ai的路标
+
     public Transform StartTransform;
     public Transform EndTransform;
 
-    public float NodeRadius=0.5f;        //节点半径
+    public float NodeRadius=1f;        //节点半径
 
     public LayerMask WallLayer;      //过滤墙体所在的层
+
+    
 
     public class NodeItem              //节点类 储存每一个节点的信息
     {
@@ -22,7 +26,7 @@ public class grid : MonoBehaviour {
         {
             get
             {
-                return gCost + fCost;
+                return gCost + hCost;
             }
         }        
         public NodeItem parent;    //父节点（中心节点）
@@ -39,7 +43,7 @@ public class grid : MonoBehaviour {
 
     private int w, h;            
 
-    private GameObject Wall;
+    //在Hierarchy中管理生成路标的物体
     private GameObject Path;
 
     private List<GameObject> PathObject = new List<GameObject>();
@@ -50,19 +54,18 @@ public class grid : MonoBehaviour {
         h = Mathf.RoundToInt(transform.localScale.y * 31);   
         Grid = new NodeItem[w, h];                           //创建对应的节点二维数组
 
-         Wall = new GameObject("WallRange");
-         Path = new GameObject("PathRange");
+        Debug.Log(w + " " + h);
+        Path = new GameObject("PathRange");
 
         for(int x=2;x<w;x++)
             for(int y = 2; y < h; y++)
             {
                 Vector3 pos = new Vector3(x, y, 0);
+                
                 bool isWall = Physics.CheckSphere(pos, NodeRadius,WallLayer);
+                Debug.Log(isWall);
                 Grid[x, y] = new NodeItem(isWall, x, y, pos);      //构建节点
-                if (isWall)
-                {
-                   //画出不可行走区域？
-                }
+                
             }
 	}
     //根据坐标位置获得对应的节点
@@ -77,8 +80,62 @@ public class grid : MonoBehaviour {
         return Grid[x, y];
     }
 	
+
+    //得到周边节点
+    public List<NodeItem> GetNearNode(NodeItem node)
+    {
+        List<NodeItem> NearList = new List<NodeItem>();
+        for(int i=-1;i<2;i++)
+            for(int j = -1; j < 2; j++)
+            {
+                if (i == 0 && j == 0)                 //跳过自身
+                    continue;
+                else
+                {
+                    int x = node.x + i;
+                    int y = node.y + j;
+                    if (x > 1 && x < w && y > 1 && y < h) //不超过边界则放到集合中
+                    {
+                        NearList.Add(Grid[x, y]);
+                    }                    
+                }
+            }
+        return NearList;
+    }
+
+    //更新路标
+    public void upadatePath(List<NodeItem> path)
+    {
+        path = new List<NodeItem>();
+        //得到新生成的路径的长度
+        int newpathlength = path.Count;
+        //当新生成的路径的长度大于场景中的路径路标的数量时       
+        if (newpathlength > PathObject.Count)
+        {
+            for(int i = PathObject.Count; i < newpathlength; i++)
+            {
+                //实例化多出来的数量的物体
+                GameObject newpathobj = Instantiate(node, path[i].pos, Quaternion.identity);
+                //方便在Hierarchy中管理
+                newpathobj.transform.SetParent(Path.transform);
+            }            
+        }
+        else
+        {
+            for(int i = 0; i < PathObject.Count; i++)
+            {
+                //激活场景中存在的未被激活的路标
+                PathObject[i].SetActive(true);
+                //更改场景中已经存在的路标的位置
+                PathObject[i].transform.position = path[i].pos;
+            }
+        }
+        for(int i = newpathlength; i < PathObject.Count; i++)
+        {
+            //使多余的路标不可见
+            PathObject[i].SetActive(false);
+        }
+    }
+    
 	
-	void Update () {
-		
-	}
 }
